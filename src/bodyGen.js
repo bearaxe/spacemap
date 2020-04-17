2bb90962c0a3ddef0215b71c9ab6d0807eace225
bearaxe <20572230+bearaxe@users.noreply.github.com>
2020-04-16 22:43:12 -0400

    add min and max

--- src/bodyGen.js
+++ src/bodyGen.js
@@ -1,13 +1,23 @@
-const createMap = (minimumPoints = 1) => {
-  return Array(minimumPoints).fill(0).map((el,idx)=>generatePoint(idx))
+const defaultConfigs = {
+  size: 1,
+  min: 0,
+  max: 99999
 }
 
-const generatePoint = (modifier) => {
+const createMap = (config = defaultConfigs) => {
+  return Array(config.size).fill(0).map((el,idx)=>generatePoint(config))
+}
+
+const generatePoint = (config) => {
   return {
-    x: 1*modifier,
-    y: 2,
-    z: 3
+    x: randomInt(config),
+    y: randomInt(config),
+    z: randomInt(config)
   }
+}
+
+const randomInt = ({min, max}) => {
+  return Math.floor(Math.random()*(max -min) + min)
 }
 
 module.exports = {
--- tests/map.spec.js
+++ tests/map.spec.js
@@ -4,40 +4,78 @@ require('mocha');
 const bodyGen = require('../src/bodyGen');
 
 /*
-STAGE 1
-when generating the map
-  it should create an array
-  the array should have location data objects
-
+Stage 2
 when generating location data
-  it should have an xyz position
-  it should never repeat exact xyz between 2 data points
+  it should generate an x y or z position between the min and max
+  it should contain the index of the next closest based on x, y, and z
 
 
-STAGE 2
--point seeding (map seeding)
+
+STAGE Future
+-point seeding (map seeding -> store the seed and regen later)
 -map chunking (pretty sure this has to be 3d)
 */
 
 describe('mapper', () => {
+  const config = {
+    size: 4,
+    min: 235,
+    max: 849
+  };
+
+  const axes = ['x', 'y', 'z'];
+
   it('should should create an array of a desired size', () => {
-    const results = bodyGen.createMap(16);
-    expect(results.length).to.equal(16);
+    const results = bodyGen.createMap(config);
+    expect(results.length).to.equal(config.size);
   })
 
   describe('location data', () => {
-    it('should have an xyz position as integers', () => {
-      const [results, ...a] = bodyGen.createMap();
-      expect(typeof results.x).to.equal('number');
-      expect(typeof results.y).to.equal('number');
-      expect(typeof results.z).to.equal('number');
-    });
-
     it('should not repeat xyz positions', () => {
-      const [first, second, third, ...theRest] = bodyGen.createMap(4);
+      const [first, second, third, ...theRest] = bodyGen.createMap(config);
       expect(first).to.not.eql(second);
       expect(first).to.not.eql(third);
       expect(second).to.not.eql(third);
     })
+
+    axes.forEach(axis => {
+      describe(axis, () => {
+        const [results, ...theRest] = bodyGen.createMap(config);
+        it(`should have an ${axis} position as an integer`, () => {
+          // const [results, ...theRest] = bodyGen.createMap();
+          expect(results[axis]).to.satisfy(Number.isInteger);
+        });
+        it(`should create a position where ${axis} is greater than max`, () => {
+          const testMin = config.min -1;
+          // const [results, ...theRest] = bodyGen.createMap(config);
+          expect(results[axis]).to.be.greaterThan(testMin);
+        })
+        it(`should create a position where ${axis} is less than max`, () => {
+          expect(results[axis]).to.be.lessThan(config.max);
+        })
+      })
+    })
+  })
+
+  xdescribe('data links', () => {
+    const defaultSet = {
+      x: 50,
+      y: 50,
+      z: 50,
+    };
+    const validLinks = [{
+      ...defaultSet
+    },
+    { ...defaultSet,
+      x: 51
+    },
+    { ...defaultSet,
+      y: 60
+    },
+    { ...defaultSet,
+      z: 75
+    },
+  ]
+
   })
 })
